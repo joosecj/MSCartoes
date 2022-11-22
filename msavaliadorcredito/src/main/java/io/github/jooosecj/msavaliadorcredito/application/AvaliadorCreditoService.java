88a33a833ee21e83bb466ad0b0e7e7c209b0dd8a
Jose Carlos <josecarloscjj@gmail.com>
2022-11-22 19:51:12 -0300

    Retornando cliente e cartoes

--- msavaliadorcredito/src/main/java/io/github/jooosecj/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/jooosecj/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,23 +1,30 @@
 package io.github.jooosecj.msavaliadorcredito.application;
 
+import io.github.jooosecj.msavaliadorcredito.clients.CartoesResourceClient;
 import io.github.jooosecj.msavaliadorcredito.clients.ClienteResourceClient;
+import io.github.jooosecj.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.jooosecj.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.jooosecj.msavaliadorcredito.domain.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
+    private final CartoesResourceClient cartoesResourceClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clienteResourceClient.getCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesByCliente = cartoesResourceClient.getCartoesByCliente(cpf);
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesByCliente.getBody())
                 .build();
     }
 
